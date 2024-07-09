06185d07be10c381cee24117a77da1e9091c83f1
Marco Antonio <marcosmarfiww252@gmail.com>
2024-07-09 12:04:05 -0300

    public

---
+++ script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const peso = document.getElementById("peso").value;
+  const altura = document.getElementById("altura").value;
+
+  const imc = (peso / altura ** 2).toFixed(2);
+
+  const value = document.getElementById("value");
+  let description = " ";
+
+  document.getElementById("infos").classList.remove("hidden");
+
+  if (imc < 18.5) {
+    description = "Você Esta abaixo do peso !!";
+    value.classList.add("moderada");
+    value.classList.remove("normal");
+    value.classList.remove("attention");
+  } else if (imc >= 18.5 && imc <= 25) {
+    description = "Você está no peso ideal!!";
+    value.classList.add("normal");
+    value.classList.remove("attention");
+    value.classList.remove("moderada");
+  } else if (imc > 25 && imc <= 30) {
+    description = "Você está com sobrepeso!";
+    value.classList.add("moderada");
+    value.classList.remove("normal");
+    value.classList.remove("attention");
+  } else if (imc > 30 && imc <= 35) {
+    description = "Você está no peso com obesidade moderada!! ";
+    value.classList.add("attention");
+    value.classList.remove("normal");
+    value.classList.remove("moderada");
+  } else if (imc > 35 && imc <= 40) {
+    description = " CUIDADO! Você está no peso com obesidade severa!! ";
+    value.classList.add("attention");
+    value.classList.remove("normal");
+    value.classList.remove("moderada");
+  } else {
+    description = " CUIDADO! Você está no peso com obesidade morbida!! ";
+    value.classList.add("attention");
+    value.classList.remove("normal");
+    value.classList.remove("moderada");
+  }
+
+  value.textContent = imc.replace(".", ",");
+  document.getElementById("description").textContent = description;
+});
